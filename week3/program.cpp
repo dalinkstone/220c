3ed0bb1cf8d5ddc681e12b12480327377f2f274c
Dalin Stone <dalinkstone@gmail.com>
2025-11-20 21:57:11 -0700

    this adds the basic list of accounts

--- week3/program.cpp
+++ week3/program.cpp
@@ -1,14 +1,14 @@
 #include "account.h"
 
 #include <iostream>
+#include <list>
 
 int main() {
   int user_input = 9;
   float deposit = 0;
   float withdraw = 0;
 
-  account::Account new_account;
-  new_account.input_info();
+  std::list<account::Account> accounts;
 
   while (user_input != 0) {
     std::cout << "\nAccount Menu:\n";
@@ -16,29 +16,36 @@ int main() {
     std::cout << "1. Display Account Information\n";
     std::cout << "2. Add a deposit to an account\n";
     std::cout << "3. Withdraw from an account\n";
+    std::cout << "4. Add new account\n";
 
     std::cout << "Your choice: ";
     std::cin >> user_input;
+
+    account::Account new_account;
 
     switch (user_input) {
     case 0:
       return EXIT_SUCCESS;
     case 1:
-      std::cout << new_account;
+      // std::cout << new_account;
       break;
     case 2:
       std::cout << "Amount to deposit: ";
       std::cin >> deposit;
 
-      new_account += deposit;
+      // new_account += deposit;
 
       break;
     case 3:
       std::cout << "Amount to withdraw: ";
       std::cin >> withdraw;
 
-      new_account -= withdraw;
+      // new_account -= withdraw;
 
+      break;
+    case 4:
+      new_account.input_info();
+      accounts.push_back(new_account);
       break;
     default:
       std::cout << "That is not a valid option. Try again.\n";
